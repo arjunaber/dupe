dc84712fa9d0db7c21ad9e281157d8c8c548a0bf
christianbryans <christianbryan263@gmail.com>
2025-04-08 23:29:17 +0700

    TELSHIP-2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisterController;
 
+Route::get('/register', [RegisterController::class, 'show'])->name('register');
+Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('register'); // atau bisa return view langsung
 });
+
+use App\Http\Controllers\RegisterMentorController;
+
+Route::get('/registermentor', [RegisterMentorController::class, 'show'])->name('registermentor');
+Route::post('/registermentor', [RegisterMentorController::class, 'store'])->name('registermentor.store');
+
+use App\Http\Controllers\LoginController;
+
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login'])->name('login.post');
+
+use App\Http\Controllers\MahasiswaController;
+
+Route::get('/mahasiswa', [MahasiswaController::class, 'index'])->name('mahasiswa.index');
+
+
