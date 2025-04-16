8dcbc3fcae6b29c47f0af05c3a493b8a139ee1a3
arjunaber <arjunaaber2@gmail.com>
2025-04-17 04:59:33 +0700

    add:  laporan_harian CRUD and FE

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Laporan;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Mahasiswa;
 
 class LaporanController extends Controller
 {
@@ -10,4 +13,23 @@ class LaporanController extends Controller
     {
         return view('mahasiswa.laporan_harian');
     }
+
+    public function store(Request $request)
+{
+    $request->validate([
+        'tanggal' => 'required|date',
+        'deskripsi' => 'required|string|max:2000',
+    ]);
+
+    Laporan::create([
+        // 'id_mahasiswa' => Auth::user()->mahasiswa->id_mahasiswa,
+        'id_mahasiswa' => 1, // sementara
+        'jenis_laporan' => 'laporan_harian',
+        'tanggal' => $request->tanggal,
+        'deskripsi_pekerjaan' => $request->deskripsi,
+        'status' => 'diproses',
+    ]);
+
+    return redirect()->back()->with('success', 'Laporan berhasil dikirim!');
+}
 }
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LaporanController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('dashboard');
-Route::get('/laporan', [App\Http\Controllers\LaporanController::class, 'index'])->name('laporan');
+Route::get('/', [HomeController::class, 'index'])->name('dashboard');
+
+
+Route::get('/laporan', [LaporanController::class, 'index'])->name('laporan');
+Route::post('/laporan', [LaporanController::class, 'store'])->name('laporan.store');
